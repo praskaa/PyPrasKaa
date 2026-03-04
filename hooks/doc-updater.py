d2cf12e17b136171aa39b0eaaa07f2173d62a542
PrasKaa <praskaa@bim.id>
2026-03-04 22:48:59 +0700

    feat(hooks): extend doc-updater to track sheet and view modifications
    Add support for OST_Sheets and OST_Views categories in the doc-updater
    hook, allowing LastModifiedBy parameter updates on sheets and views in
    addition to family instances. Also includes style fixes for encoding
    declaration and code formatting improvements.

--- hooks/doc-updater.py
+++ hooks/doc-updater.py
@@ -1,52 +1,56 @@
-#*-* coding:utf-8 *-*
+# -*- coding: utf-8 -*-
 from datetime import datetime
-from Autodesk.Revit.DB import (BuiltInCategory, BuiltInParameter, ElementId, WorksharingUtils, FamilyInstance)
+from Autodesk.Revit.DB import (
+    BuiltInCategory, BuiltInParameter, ElementId,
+    WorksharingUtils, FamilyInstance,
+    ViewSheet, View  # ← tambahkan import ini
+)
 
-# Variables
-sender  = __eventsender__
-args    = __eventargs__
+sender         = __eventsender__
+args           = __eventargs__
+doc            = args.GetDocument()
 
-doc     = args.GetDocument()
-
-# Get the modified, deleted and new elements
 modified_el_ids = args.GetModifiedElementIds()
-deleted_el_ids  = args.GetDeletedElementIds()
-new_el_ids      = args.GetAddedElementIds()
+modified_el     = [doc.GetElement(e_id) for e_id in modified_el_ids]
 
-modified_el = [doc.GetElement(e_id) for e_id in modified_el_ids]
-
-
-# IUpdater - Modified Elements
-allowed_cats =[
+allowed_cats = [
     ElementId(BuiltInCategory.OST_StructuralFraming),
-    ElementId(BuiltInCategory.OST_StructuralColumns), 
-    ElementId(BuiltInCategory.OST_StructuralFoundation), 
-    ElementId(BuiltInCategory.OST_Walls), 
+    ElementId(BuiltInCategory.OST_StructuralColumns),
+    ElementId(BuiltInCategory.OST_StructuralFoundation),
+    ElementId(BuiltInCategory.OST_Walls),
     ElementId(BuiltInCategory.OST_Floors),
-    ElementId(BuiltInCategory.OST_Stairs)
+    ElementId(BuiltInCategory.OST_Stairs),
+    ElementId(BuiltInCategory.OST_Sheets),  # ← Sheet
+    ElementId(BuiltInCategory.OST_Views),   # ← View
 ]
-for el in modified_el:
 
-    if type(el) != FamilyInstance:
+for el in modified_el:
+    # Ganti filter: izinkan FamilyInstance ATAU ViewSheet ATAU View
+    is_family   = isinstance(el, FamilyInstance)
+    is_sheet    = isinstance(el, ViewSheet)
+    is_view     = isinstance(el, View) and not isinstance(el, ViewSheet)
+
+    if not (is_family or is_sheet or is_view):
         continue
 
-    # Check if element is in allowed categories
-    if el.Category.Id in allowed_cats:
+    # Cek kategori (opsional untuk sheet/view karena sudah dicek tipenya)
+    el_cat = el.Category
+    in_allowed_cat = el_cat is not None and el_cat.Id in allowed_cats
 
-        try:
-            # Get the last modified date
-            timestamp = datetime.now()
-            f_timestamp = timestamp.strftime(r"%a, %d %b %H:%M")
+    if not (in_allowed_cat or is_sheet or is_view):
+        continue
 
-            wti = WorksharingUtils.GetWorksharingTooltipInfo(doc, el.Id)
-            last = wti.LastChangedBy
+    try:
+        timestamp   = datetime.now()
+        f_timestamp = timestamp.strftime(r"%a, %d %b %H:%M")
+        wti         = WorksharingUtils.GetWorksharingTooltipInfo(doc, el.Id)
+        last        = wti.LastChangedBy
+        value       = "{} ({})".format(last, f_timestamp)
 
-            value   = "{} ({})".format(last, f_timestamp)
+        p_last = el.LookupParameter('LastModifiedBy')
+        if p_last and not p_last.IsReadOnly:
+            p_last.Set(value)
 
-            p_last  = el.LookupParameter('LastModifiedBy')
-            if p_last:
-                p_last.Set(value)
- 
-        except:
-            import traceback
-            print(traceback.format_exc())
+    except:
+        import traceback
+        print(traceback.format_exc())
